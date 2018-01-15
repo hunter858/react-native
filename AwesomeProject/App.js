99f6c52d206fa23f9272b9cc3a29c3440d6d6977
pengchao <pengchao@inner.sss.com>
2018-01-15 19:28:14 +0800

    添加tabbar 成功设置一些属性

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -11,7 +11,16 @@ import {
   Text,
   View,
   TabBarIOS,
+  Dimensions,
 } from 'react-native';
+
+
+// import Home  from '.main/Home';
+
+const ScreenWidth = Dimensions.get('window').width; //屏幕宽
+const ScreenHeight = Dimensions.get('window').height; //高
+const ScreenScale = Dimensions.get('window').scale;//比例
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -21,12 +30,31 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+
+
   render() {
     return (
       <View style={styles.container}>
         <TabBarIOS
-          style={{height:49,width:width}}
+          style={{height:49,width:ScreenWidth}}
+          tintColor="green" //被选中的标签的颜色
+          barTintColor="black" //tabbarIOS 背景色
+          translucent={false}     // TabBarIOS不需要半透明效果
           >
+            <TabBarIOS.Item
+              systemIcon="bookmarks"
+              badge="15"
+              title="首页"  //如果是系统图标，则标题不会出现
+              //icon={require('.image!home')} //自定义图标- 目前只支持本地图片
+              //selectedIcon={require('image!baker')} //自定义高亮图标
+              >
+            </TabBarIOS.Item>
+            <TabBarIOS.Item systemIcon="contacts">
+            </TabBarIOS.Item>
+            <TabBarIOS.Item systemIcon="downloads">
+            </TabBarIOS.Item>
+            <TabBarIOS.Item systemIcon="favorites">
+            </TabBarIOS.Item>
         </TabBarIOS>
       </View>
     );
